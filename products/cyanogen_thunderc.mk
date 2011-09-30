a55b6c846474d643c575dfde5d8e209c3e694e7c
Alex Zepeda <alex@inferiorhumanorgans.com>
2011-09-30 12:13:04 -0700

    thunderc: Bump to CM 7.1.0 final.

--- products/cyanogen_thunderc.mk
+++ products/cyanogen_thunderc.mk
@@ -35,10 +35,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC1-IHO
+            ro.modversion=CyanogenMod-7.1.0-IHO
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC1-IHO-KANG
+            ro.modversion=CyanogenMod-7.1.0-IHO-KANG
     endif
 endif
 
